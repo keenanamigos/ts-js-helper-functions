043dad631fa13522b8761efd2661d3e3d6b6261a
Keenan Johnson <keenan.johnson1@outlook.com>
2017-12-18 15:31:08 -0600

    Add unit test for getFormattedCurrentDate function

--- test/DateHelper.spec.ts
+++ test/DateHelper.spec.ts
@@ -2,6 +2,21 @@ import { expect } from "chai";
 import { DateHelper } from "../src/DateHelper";
 
 describe("DateHelper", () => {
+    describe("getFormattedCurrentDate", () => {
+        it("should return the current date as a string formatted as mm/dd//yyyy", () => {
+            const currentDate: Date = new Date();
+
+            // January is 0 by default so 1 is added for formattting
+            const month: number = currentDate.getMonth() + 1;
+            const day: number = currentDate.getDate();
+            const year: number = currentDate.getFullYear();
+
+            const formattedDate: string = DateHelper.formatDate(month, day, year);
+
+            expect(formattedDate).to.equal(DateHelper.getFormattedCurrentDate());
+        });
+    });
+
     describe("formatDate", () => {
         it("should return a properly formatted date", () => {
             const month = 8;
