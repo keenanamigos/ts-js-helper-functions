ce77ef920af03b669542f0fc7c219bbc342b6e59
Keenan Johnson <keenan.johnson1@outlook.com>
2017-12-18 18:36:45 -0600

    Add ObjectHelper class, Add isObjectNullOrEmpty function

---
+++ dist/ObjectHelper.js
@@ -0,0 +1,30 @@
+"use strict";
+/**
+ * @class ObjectHelper
+ * Class to perform checks and operations on objects.
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+var ObjectHelper = /** @class */ (function () {
+    function ObjectHelper() {
+    }
+    /**
+     * @function
+     * Perform a check to see if a given object is null or empty.
+     * @param {object} object - The object to be checked.
+     * @returns {boolean} - Return a boolean dependent on whether the given object was null or empty.
+     */
+    ObjectHelper.isObjectNullOrEmpty = function (object) {
+        if (object === null || typeof object === "undefined") {
+            return true;
+        }
+        if (object.constructor !== Object) {
+            throw new TypeError(object + " is not an Object.");
+        }
+        if (Object.keys(object).length === 0 && object.constructor === Object) {
+            return true;
+        }
+        return false;
+    };
+    return ObjectHelper;
+}());
+exports.ObjectHelper = ObjectHelper;
---
+++ src/ObjectHelper.ts
@@ -0,0 +1,28 @@
+/**
+ * @class ObjectHelper
+ * Class to perform checks and operations on objects.
+ */
+
+ export class ObjectHelper {
+     /**
+      * @function
+      * Perform a check to see if a given object is null or empty.
+      * @param {object} object - The object to be checked.
+      * @returns {boolean} - Return a boolean dependent on whether the given object was null or empty. 
+      */
+    public static isObjectNullOrEmpty(object: any): boolean {
+        if (object === null || typeof object === "undefined") {
+            return true;
+        }
+
+        if (object.constructor !== Object) {
+            throw new TypeError(`${object} is not an Object.`);
+        }
+
+        if (Object.keys(object).length === 0 && object.constructor === Object) {
+            return true;
+        }
+
+        return false;
+    }
+ }
